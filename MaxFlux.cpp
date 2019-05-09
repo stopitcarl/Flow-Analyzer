82abb15c051e065c3fa055a616f66283bd23be1d
João Porto <joaofporto@tecnico.ulisboa.pt>
2019-05-09 15:55:12 +0100

    Code cleanup

--- MaxFlux.cpp
+++ MaxFlux.cpp
@@ -1,13 +1,15 @@
-#include <vector>
-#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
 #include <list>
 #include <string>
-#include <iostream>
-#include <cmath>
+#include <vector>
+#include <queue>
 
 #define MIN(a, b) (a < b ? a : b)
 #define HEIGHT(a) (nodes[a]->getHeight())
 #define IS_STATION(a) (a >= (2 + suppliers))
+#define IS_STATION_OUT(a) (a >= (2 + suppliers + storage))
 
 using namespace std;
 
@@ -16,7 +18,7 @@ class Node;
 int suppliers;        // Number of suppliers
 int storage;          // Number of storage
 vector<Node *> nodes; // Vector of nodes
-queue<Node *> L;      // Queue for Relabel to front
+queue<int> L;         // Queue for Relabel to front
 bool *found;          // Array that contains the direct parent of each vertex
 bool *isInL;          // Checklist if a vertex is in the stack
 
@@ -60,11 +62,15 @@ public:
     }
     ~Node()
     {
+        // No freeing up memory cause why waste time
         /*for (Edge *edge : _connects)
             delete edge;*/
     }
 };
 
+/*
+Returns: the edge to lowest node
+*/
 Edge *relabel(Node *node)
 {
     vector<Edge *> adjs = node->getConnections();
@@ -85,6 +91,9 @@ Edge *relabel(Node *node)
     return edge;
 }
 
+/*
+Returns: amount of excess pushed trough edge e
+*/
 int push(Edge *e, int *push)
 {
 
@@ -96,9 +105,9 @@ int push(Edge *e, int *push)
     e->cap -= amountPushed;
     nodes[e->dest]->addExcess(amountPushed);
 
-    if (!isInL[e->dest] && amountPushed > 0)
+    if (!isInL[e->dest])
     {
-        L.push(nodes[e->dest]);
+        L.push(e->dest);
         isInL[e->dest] = true;
     }
 
@@ -108,10 +117,10 @@ int push(Edge *e, int *push)
 void relabelToFront()
 {
     // Create list L
-    L = queue<Node *>();
+    L = queue<int>();
     for (int i = 0; i < suppliers; i++)
     {
-        L.push(nodes[2 + i]);
+        L.push(2 + i);
         isInL[2 + i] = true;
     }
 
@@ -119,7 +128,7 @@ void relabelToFront()
     Node *node;
     while (!L.empty())
     {
-        node = L.front();
+        node = nodes[L.front()];
         adjs = node->getConnections();
         int *excess = node->getExcess();
         while (*excess > 0)
@@ -131,8 +140,8 @@ void relabelToFront()
                 if (e->cap > 0 && HEIGHT(e->dest) < node->getHeight()) // IF: edge has capacity and is lower
                     push(e, excess);                                   // THEN: push excess through it
 
-                if (*excess == 0)
-                    break;
+                if (*excess == 0) // IF: There's nor more excess to push
+                    break;        // THEN: Leave loop
             }
 
             if (*excess > 0)
@@ -142,7 +151,6 @@ void relabelToFront()
         isInL[node->getIndex()] = false;
         L.pop();
     }
-    //cout << " Done with RELABEL TO FRONT" << endl;
 }
 
 int checkOutGoingFlow(Node *node)
@@ -160,22 +168,20 @@ bool compareEdges(int *&first, int *&second)
 
 void bfs(int startingNode)
 {
-    // Mark all the vertices as not visited
-
     // Create a queue for BFS
     queue<int> queue;
 
-    // Mark the current node as visited and enqueue it
+    // Mark the sink node as visited
     found[startingNode] = found[0] = true;
-
-    bool augStations[2 + suppliers + storage * 2]{0};
-
     queue.push(startingNode);
+
+    // Create bool array to check if a given node is an augmented station
+    bool augStations[2 + suppliers + storage * 2]{0};
 
     // List of adjencies
     vector<Edge *> edges;
     list<int *> minimumCut = list<int *>();
-    list<int> stations = list<int>();
+    deque<int> stations = deque<int>();
 
     while (!queue.empty())
     {
@@ -189,7 +195,7 @@ void bfs(int startingNode)
             {
                 if (edge->cap > 0 && edge->back->cap == 0)
                 {
-                    if (startingNode >= (suppliers + 2) && edge->dest >= (suppliers + 2) && abs(edge->dest - startingNode) == storage)
+                    if (IS_STATION(startingNode) && IS_STATION(edge->dest) && abs(edge->dest - startingNode) == storage)
                         augStations[startingNode] = augStations[edge->dest] = true;
 
                     minimumCut.push_back(new int[2]{edge->dest, startingNode});
@@ -213,27 +219,25 @@ void bfs(int startingNode)
             it = minimumCut.erase(it);
             --it;
         }
-        else if (edge[0] >= (suppliers + 2) && edge[1] >= (suppliers + 2) && abs(edge[1] - edge[0]) == storage) // If it's a station-to-station edge
+        else if (IS_STATION(edge[0]) && IS_STATION(edge[1]) && abs(edge[1] - edge[0]) == storage) // If it's a station-to-station edge
         {
             stations.push_back(MIN(edge[0], edge[1]));
             it = minimumCut.erase(it);
             --it;
         }
-        else if (augStations[edge[0]] || augStations[edge[1]]) // ignore this
+        else if (augStations[edge[0]] || augStations[edge[1]]) // If either one of its nodes is an augmented station
         {
             it = minimumCut.erase(it);
             --it;
         }
-        else if (edge[0] >= suppliers + 2 + storage)
+        else if (IS_STATION_OUT(edge[0]))
             edge[0] -= storage;
-        //        else if (edge[1] >= suppliers + 2 + storage)
-        //          edge[1] -= storage;
     }
 
     if (!stations.empty())
     {
-        stations.sort();
-        list<int>::iterator it = stations.begin();
+        auto it = stations.begin();
+        sort(it, stations.end());
         cout << *it;
         ++it;
         while (it != stations.end())
@@ -245,10 +249,10 @@ void bfs(int startingNode)
     cout << endl;
 
     minimumCut.sort(compareEdges);
-    for (auto edge : minimumCut)
+    for (int *edge : minimumCut)
         cout << edge[0] << " " << edge[1] << endl;
 }
-
+/*
 void printNode(Node *node)
 {
     vector<Edge *> edges = node->getConnections();
@@ -266,12 +270,11 @@ void printStatus(vector<Node *> nodes)
     for (int i = 0; i < size; i++)
         printNode(nodes[i]);
 }
-
+*/
 int readInput()
 {
     int nodesNum;
     int edges;
-    //cout << "reading line 1 (suppliers, gas stations, number of roads)" << endl;
 
     // Read the number of vertexes and edges
     if (!scanf("%u %u %u", &suppliers, &storage, &edges))
@@ -288,7 +291,6 @@ int readInput()
     nodes[0]->setHeight(nodes.size());
 
     // Read suppliers capacity
-    //cout << "reading line 2 (suppliers)" << endl;
     Edge *edge, *backEdge;
     for (int i = 0; i < suppliers; i++)
     {
@@ -306,8 +308,7 @@ int readInput()
         nodes[2 + i]->addConnection(backEdge); // Part of residual graph
     }
 
-    //cout << "reading line 3 (gas stations capacity)" << endl;
-    // Read mid-way stations' capacity
+    // Read storage stations capacity
     for (int i = 0; i < storage; i++)
     {
         edge = new Edge, backEdge = new Edge;
@@ -322,33 +323,30 @@ int readInput()
         edge->dest = storage_out;
         backEdge->dest = storage_in;
         backEdge->cap = 0;
-
+        // Add edges to nodes
         nodes[storage_in]->addConnection(edge);
         nodes[storage_out]->addConnection(backEdge);
     }
 
     // Read road network
-    //cout << "reading line 4 and onwards (roads)" << endl;
     int origin;
     edge = new Edge, backEdge = new Edge;
     while (scanf("%u %u %u", &origin, &edge->dest, &edge->cap) > 0)
     {
+        if (origin >= 2 + suppliers) // IF: edge leaves from storage
+            origin += storage;       // THEN: offset storage's index to its 'out' node
 
-        if (origin >= 2 + suppliers) // If edge goes out of storage for storage_in and storage_out
-            origin += storage;
-
-        backEdge->dest = origin;
-
+        // Connect two edges
         edge->back = backEdge;
         backEdge->back = edge;
-
+        // Initialize the backedge
+        backEdge->dest = origin;
         backEdge->cap = 0;
-
+        // Add edges to nodes
         nodes[origin]->addConnection(edge);
         nodes[edge->dest]->addConnection(backEdge);
         edge = new Edge, backEdge = new Edge;
     }
-    delete edge, delete backEdge;
 
     return nodesNum;
 }
@@ -361,26 +359,15 @@ int main()
     isInL[0] = isInL[1] = true; // Kids, here's a little lesson in trickery
     found = new bool[nodesNum]{0};
 
-    //    printStatus(nodes);
-
-    /*printNode(nodes[37]);
-    printNode(nodes[37 + storage]);
-    printNode(nodes[47]);
-    printNode(nodes[47 + storage]);
-    */
     //    cout << "################## RELABEL TO FRONT ##########################" << endl;
     relabelToFront();
-    /*
-    printNode(nodes[37]);
-    printNode(nodes[37 + storage]);
-    printNode(nodes[47]);
-    printNode(nodes[47 + storage]);
-*/
+
     cout << checkOutGoingFlow(nodes[1]) << endl; // Print max flow
     bfs(1);
 
+    /* 
     // Keep this code to free nodes
-    /*    for (Node *node : nodes)
+       for (Node *node : nodes)
         delete node;
 
     delete[] isInL;
